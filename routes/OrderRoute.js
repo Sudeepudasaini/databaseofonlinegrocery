1925d200a88194cd752404d47b38a54f2e149f5c
Sudeeppudasaini <heroclan82@gmail.com>
2025-05-04 21:09:34 +0545

    it is new

---
+++ routes/OrderRoute.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import authUser from '../middleware/authUser.js';
+import { placeOrderCOD } from '../controllers/orderController.js';
+
+const orderRouter= express.Router();
+
+orderRouter.post('/cod', authUser, placeOrderCOD)
